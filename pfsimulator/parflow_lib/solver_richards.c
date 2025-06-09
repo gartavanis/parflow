27202a7811d1eb661ea58d4fe2afa576879038dc
George Artavanis <ga6@verde.princeton.edu>
2025-06-08 23:15:03 -0400

    Fix memory leaks

--- pfsimulator/parflow_lib/solver_richards.c
+++ pfsimulator/parflow_lib/solver_richards.c
@@ -1320,6 +1320,8 @@ SetupRichards(PFModule * this_module)
       }
       FreeVector(sres);
       FreeVector(ssat);
+      FreeVector(alpha);
+      FreeVector(n);
       EndTiming(TorchTimingIndex);
     }
 #endif
